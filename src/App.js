647df0f062a4f678ce6db6dbfc95854330560a70
GITSHOK <nishokganapathy07@gmail.com>
2025-09-14 13:17:12 +0530

    bg animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Posts from "./pages/Posts";
 import About from "./pages/About";
 import ContactForm from "./pages/ContactForm";
 import Post from "./pages/Post";
+import "./App.css";
+
+// Background component with different animations for each route
+function Background() {
+  const location = useLocation();
+  
+  return (
+    <div className={`background ${location.pathname === '/' ? 'home-bg' : ''} 
+                    ${location.pathname === '/about' ? 'about-bg' : ''} 
+                    ${location.pathname === '/contact' ? 'contact-bg' : ''}`}>
+      <div className="background-animation"></div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/post/:id" element={<Post/>}/>
-        </Routes>
+      <div className="app-container">
+        <Background />
+        <Navbar />
+        <div className="content-wrapper">
+          <Routes>
+            <Route path="/" element={<Posts />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactForm />} />
+            <Route path="/post/:id" element={<Post/>}/>
+          </Routes>
+        </div>
       </div>
     </Router>
   );
